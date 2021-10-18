0a0b3a474a8e87c71a429a19903f343281bee265
Mitchell Carroll <mjpc815@gmail.com>
2021-10-17 20:26:14 -0400

    Mitchell's stubs

---
+++ JobListingsList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class JobListingsList {
+    
+    private ArrayList<JobListing> jobListings;
+
+    public JobListingsList(ArrayList<JobListing> jobListings) {
+
+    }
+
+    public void addListing(JobListing jobListing, Employer employer) {
+        
+    }
+
+    public ArrayList<JobListing> searchListings(String keyWord) {
+        
+    }
+
+    public ArrayList<JobListing> sortListings(SortType sortType) {
+
+    }
+}
---
+++ Review.java
@@ -0,0 +1,15 @@
+
+public class Review {
+    
+    private int rating;
+    private String review;
+    private User reviewer;
+
+    public Review(int rating, String review, User reviewer) {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+}
