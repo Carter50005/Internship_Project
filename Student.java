4f5f06f82add505315cace625a55a144ea0e9300
Colton-Anderson <coltonanderson10@gmail.com>
2021-10-21 13:56:31 -0400

    student

--- Student.java
+++ Student.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 
 public class Student extends User {
-
-    private String uUID;
     private String firstName;
     private String lastName;
     private String email;
@@ -12,7 +10,6 @@ public class Student extends User {
 
     public Student(String username, String password, String studentID, String firstName, String lastName, String email) {
         super(username, password, 's', studentID);
-        this.uUID = studentID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -23,7 +20,6 @@ public class Student extends User {
 
     public Student(String username, String password, String studentID, String firstName, String lastName, String email, ArrayList<Resume> resumes, ArrayList<Review> reviews, ArrayList<JobListing> wishList)  {
         super(username, password, 's', studentID);
-        this.uUID = studentID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -65,6 +61,14 @@ public class Student extends User {
 
     public String getFirstName() {
         return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    private String getEmail() {
+        return this.
     }
  
 }
