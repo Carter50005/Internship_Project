02a521236a1c83c0b24bacfa1839fb352119e513
Colton-Anderson <coltonanderson10@gmail.com>
2021-11-02 19:25:17 -0400

    commit

--- JobListingsList.java
+++ JobListingsList.java
@@ -1,3 +1,6 @@
+/**
+ * Creates the Job Listing List
+ */
 import java.util.ArrayList;
 
 public class JobListingsList {
@@ -5,6 +8,9 @@ public class JobListingsList {
     private static ArrayList<JobListing> jobListings;
     private static JobListingsList jobListingList;
 
+    /**
+     * constructor
+     */
     public JobListingsList() {
         if(DataLoader.getJobListings() != null) {
             jobListings = DataLoader.getJobListings();
@@ -15,6 +21,10 @@ public class JobListingsList {
         setEmployers(DataLoader.getEmployers());
     }
 
+    /**
+     * creates a list of employers
+     * @param employers
+     */
     public void setEmployers(ArrayList<Employer> employers) {
         for(Employer employer : employers) {
             for(JobListing listing : jobListings) {
@@ -25,22 +35,39 @@ public class JobListingsList {
         }
     }
 
+    /**
+     * returns an instance of the job listing list
+     * @return
+     */
     public static JobListingsList getInstance() {
         if(jobListingList == null) {
             return new JobListingsList();
         }
         return jobListingList;
     }
+
+    /**
+     * returns the joblistings
+     * @return
+     */
     public ArrayList<JobListing> getJobListings() {
         return jobListings;
     }
 
+    /**
+     * adds a job Listing
+     * @param jobListing
+     */
     public void addListing(JobListing jobListing) {
         if(!contains(jobListing)) {
             jobListings.add(jobListing);
         }
     }
 
+    /**
+     * updates the jobListings
+     * @param listing
+     */
     public void updateListing(JobListing listing) {
         for(JobListing jobListing : jobListings) {
             if(jobListing.getUUID().equalsIgnoreCase(listing.getUUID())) {
@@ -49,6 +76,11 @@ public class JobListingsList {
         }
     }
 
+    /**
+     * Searches for a job listing using key words
+     * @param keyWord
+     * @return
+     */
     public ArrayList<JobListing> searchListings(String keyWord) {
         ArrayList<JobListing> ret = new ArrayList<JobListing>();
         for(int i=0;i<jobListings.size();i++) {
@@ -59,6 +91,11 @@ public class JobListingsList {
         return ret;
     }
 
+    /**
+     * checks if a job is already listed
+     * @param jobListing
+     * @return
+     */
     public boolean contains(JobListing jobListing) {
         for(int i=0;i<jobListings.size();i++) {
             if(jobListings.get(i) == jobListing) {
@@ -68,6 +105,12 @@ public class JobListingsList {
         return false;
     }
 
+    /**
+     * checks if a keword is within a string
+     * @param string
+     * @param keyWord
+     * @return
+     */
     public boolean containsString(String string, String keyWord) {
         if(string.contains(keyWord)) {
             return true;
@@ -75,6 +118,9 @@ public class JobListingsList {
         return false;
     }
 
+    /**
+     * Deletes the job Listing from the list
+     */
     public void deleteListing(JobListing jobListing) {
         jobListings.remove(jobListing);
     }
